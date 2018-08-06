e0e35871dde5f67937c7d3ea2f3cc78b8093e9ca
Sunil Patil <sunil.patil@gmx.de>
2018-08-07 01:05:18 +0200

    Added i18n capability

---
+++ src/main/java/com/devopsbuddy/config/I18nConfig.java
@@ -0,0 +1,18 @@
+package com.devopsbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class I18nConfig {
+
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		// Checks for new message every 30 minutes
+		messageSource.setCacheSeconds(1800);
+		return messageSource;		 
+	}
+}
--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,29 @@
 package com.devopsbuddy;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.devopsbuddy.web.i18n.I18nService;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
 
+	@Autowired
+	private I18nService i18nService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() {
+		final String expected = "Bootstrap starter template";
+		final String messageId="index.main.callout";
+		final String actual = i18nService.getMessage(messageId);
+		Assert.assertEquals("The actual and expected Strings don't match", expected, actual);
 	}
 
 }
