198caeb29da4d8f39a2245eb9ce64acd21625e33
Sunil Patil <sunil.patil@gmx.de>
2018-08-07 02:07:34 +0200

    Added about page

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.devopsbuddy.web.controllers.advices;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
