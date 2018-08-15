9e98b8a614771ef80bd94285a9373e295dfebdc0
Sunil Patil <sunil.patil@gmx.de>
2018-08-16 00:34:55 +0200

    completed S3 service

--- src/main/java/com/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfig.java
@@ -1,10 +1,17 @@
 package com.devopsbuddy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 @PropertySource("file:///${user.home}/.devopsbuddy/application-common.properties")
@@ -12,4 +19,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackages = "com.devopsbuddy.backend.persistence.domain.backend")
 @EnableTransactionManagement
 public class ApplicationConfig {
+
+	@Value("${aws.s3.profile}")
+	private String awsProfileName;
+
+	@Bean
+	public AmazonS3 s3Client() {		
+		return AmazonS3ClientBuilder.standard()
+				.withCredentials(new ProfileCredentialsProvider(awsProfileName))
+				.withRegion(Regions.EU_CENTRAL_1)
+				.build();
+	}
 }
